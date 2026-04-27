7cfd45be6b7eb0f34abac8b9cdf5df4bb0a6ebd9
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-28 02:07:43 +0530

    worked on the logic part of the theme section now be working on the ui part

--- src/app/(root)/_components/ThemeSelector.tsx
+++ src/app/(root)/_components/ThemeSelector.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { THEMES } from '../_constants';
+import { useCodeEditorStore } from '@/src/store/useCodeEditorStore';
 
 const ThemeSelector = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const mounted = useMounted();
+  // const mounted = useMounted();
   const { theme, setTheme } = useCodeEditorStore();
   const dropdownRef = useRef<HTMLDivElement>(null);
   const currentTheme = THEMES.find((t) => t.id === theme);
+
+    useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+
   return (
     <div>ThemeSelector</div>
   )
