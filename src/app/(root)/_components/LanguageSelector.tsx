a61ad2a9da7675f2c7768d86d57a62371c7bf554
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-28 02:18:41 +0530

    done with the logic part of the languageselector now bbe moving to the ui

--- src/app/(root)/_components/LanguageSelector.tsx
+++ src/app/(root)/_components/LanguageSelector.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+'use client'
+import { useCodeEditorStore } from '@/src/store/useCodeEditorStore';
+import React, { useEffect, useRef, useState } from 'react'
+import { LANGUAGE_CONFIG } from '../_constants';
 
 const LanguageSelector = ({ hasAccess }: { hasAccess: boolean }) => {
+    const [isOpen, setIsOpen] = useState(false);
+  // const mounted = useMounted();
+
+  const { language, setLanguage } = useCodeEditorStore();
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const currentLanguageObj = LANGUAGE_CONFIG[language];
+
+   useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+
+    const handleLanguageSelect = (langId: string) => {
+    if (!hasAccess && langId !== "javascript") return;
+
+    setLanguage(langId);
+    setIsOpen(false);
+  };
   return (
-    <div>LanguageSelector</div>
+    <div>
+
+    </div>
   )
 }
 
