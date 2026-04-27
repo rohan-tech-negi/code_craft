f92935958faa52e8c92cd8e0d8fd23973e3805ad
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-27 21:31:20 +0530

    now be working on the page..tsx

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,21 @@
+import { SignInButton, SignOutButton, SignUpButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+
+export default async function Home() {
+  const { userId } = await auth();
+
+  return (
+    <div>
+      <div className="min-h-screen">
+      <div className="max-w-[1800px] mx-auto p-4">
+        <Header />
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <EditorPanel />
+          <OutputPanel />
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
