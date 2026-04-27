2ed2dda7b80d30eae90673e06eada29a35305aa5
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-27 20:33:46 +0530

    as wroking on the first http.route

--- convex/http.ts
+++ convex/http.ts
@@ -1,5 +1,28 @@
 import { httpRouter } from "convex/server";
 
+import {httpAction} from "./_generated/server";
+
+const http = httpRouter();
 
 
-const http = httpRouter()
+http.route({
+    path: "/clerk-webhook",
+    method: "POST",
+    handler: httpAction(async(ctx, req)) => {
+       const webhookSecret = process.env.CLERK_WEBHOOK_SECRET;
+
+       if(!webhookSecret) {
+           throw new Error("CLERK_WEBHOOK_SECRET is not defined");
+       }
+
+        const svix_id = request.headers.get("svix-id");
+    const svix_signature = request.headers.get("svix-signature");
+    const svix_timestamp = request.headers.get("svix-timestamp");
+
+      if (!svix_id || !svix_signature || !svix_timestamp) {
+      return new Response("Error occurred -- no svix headers", {
+        status: 400,
+      });
+    }
+    }
+})
