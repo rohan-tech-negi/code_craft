213713f8ebb9db7fb0f9ac20e33b29ce83959545
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-28 02:06:16 +0530

    working on the theme selector

--- src/app/(root)/_components/ThemeSelector.tsx
+++ src/app/(root)/_components/ThemeSelector.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const ThemeSelector = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const mounted = useMounted();
+  const { theme, setTheme } = useCodeEditorStore();
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const currentTheme = THEMES.find((t) => t.id === theme);
   return (
     <div>ThemeSelector</div>
   )
