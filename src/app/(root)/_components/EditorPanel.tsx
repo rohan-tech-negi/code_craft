d94b59ea5435106ce181338c8e4738a63df780cc
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-28 02:34:00 +0530

    working on the logic section of the editorpanel

--- src/app/(root)/_components/EditorPanel.tsx
+++ src/app/(root)/_components/EditorPanel.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+'use client'
+
+import useMounted from '@/src/hooks/useMounted';
+import { useCodeEditorStore } from '@/src/store/useCodeEditorStore';
+import { useClerk } from '@clerk/nextjs';
+import React, { useEffect, useState } from 'react'
+import { LANGUAGE_CONFIG } from '../_constants';
 
 const EditorPanel = () => {
     const clerk = useClerk();
   const [isShareDialogOpen, setIsShareDialogOpen] = useState(false);
   const { language, theme, fontSize, editor, setFontSize, setEditor } = useCodeEditorStore();
+
+    const mounted = useMounted();
+
+     useEffect(() => {
+    const savedCode = localStorage.getItem(`editor-code-${language}`);
+    const newCode = savedCode || LANGUAGE_CONFIG[language].defaultCode;
+    if (editor) editor.setValue(newCode);
+  }, [language, editor]);
+
+   useEffect(() => {
+    const savedFontSize = localStorage.getItem("editor-font-size");
+    if (savedFontSize) setFontSize(parseInt(savedFontSize));
+  }, [setFontSize]);
   return (
     <div>EditorPanel</div>
   )
