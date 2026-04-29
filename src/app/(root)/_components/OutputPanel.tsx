f08e4f0f4c0704515a0c0df7e1b06ecbe03c183d
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-29 11:50:28 +0530

    working on the output panel

--- src/app/(root)/_components/OutputPanel.tsx
+++ src/app/(root)/_components/OutputPanel.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+'use client'
+import { useCodeEditorStore } from '@/src/store/useCodeEditorStore';
+import React, { useState } from 'react'
 
 const OutputPanel = () => {
+    const { output, error, isRunning } = useCodeEditorStore();
+  const [isCopied, setIsCopied] = useState(false);
   return (
     <div>OutputPanel</div>
   )
