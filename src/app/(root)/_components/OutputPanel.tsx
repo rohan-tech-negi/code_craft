bdc6ff239ea211d79e4fe360a3c1e7fe21ffd880
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-29 11:51:40 +0530

    completed the logic part of the code and working on the ui part

--- src/app/(root)/_components/OutputPanel.tsx
+++ src/app/(root)/_components/OutputPanel.tsx
@@ -5,6 +5,18 @@ import React, { useState } from 'react'
 const OutputPanel = () => {
     const { output, error, isRunning } = useCodeEditorStore();
   const [isCopied, setIsCopied] = useState(false);
+
+  
+  const hasContent = error || output;
+
+  
+  const handleCopy = async () => {
+    if (!hasContent) return;
+    await navigator.clipboard.writeText(error || output);
+    setIsCopied(true);
+
+    setTimeout(() => setIsCopied(false), 2000);
+  };
   return (
     <div>OutputPanel</div>
   )
