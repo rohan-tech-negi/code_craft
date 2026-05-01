1e5c55767f8cee759b61126146d946d03ff94c07
rohan-tech-negi <rohanwork953@gmail.com>
2026-05-01 06:00:33 +0530

    starting with todays work via run button .tsx

--- src/app/(root)/_components/RunButton.tsx
+++ src/app/(root)/_components/RunButton.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const RunButton = () => {
+   const { user } = useUser();
+  const { runCode, language, isRunning } = useCodeEditorStore();
+  const saveExecution = useMutation(api.codeExecutions.saveExecution);
   return (
     <div>RunButton</div>
   )
